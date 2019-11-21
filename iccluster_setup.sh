fad283ee064d25ed96a583775a88cc3b205abd8e
AlekseiTriastcyn <aleksei.triastcyn@epfl.ch>
2019-11-21 18:45:59 +0100

    Fix CUDA installation

--- iccluster_setup.sh
+++ iccluster_setup.sh
@@ -13,12 +13,10 @@ mkdir /home/downloads
 cd /home/downloads
 
 # download and install CUDA
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo dpkg -i cuda-repo-ubuntu1804_10.1.105-1_amd64.deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update
-sudo apt-get -y install cuda
+sudo apt-get install cuda-10.1
 
 # install python packages for machine learning
 yes | pip2 install --upgrade pip
